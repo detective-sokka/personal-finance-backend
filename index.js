e5c641fa889ad491aa52621d26ca67573cdcfc50
detectivesokka <dutt.sa@northeastern.edu>
2024-05-19 19:32:09 -0400

    Basic app

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const bcrypt = require('bcrypt');
+
+const app = express();
+const PORT = 3000;
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'mydb',
+    multipleStatements: true,
+});
+
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('MySQL Connected');
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Login route
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    db.query('SELECT * FROM users WHERE username = ?', [username], async (error, results) => {
+        if (error) {
+            throw error;
+        }
+
+        if (results.length > 0) {
+            const user = results[0];
+            const match = await bcrypt.compare(password, user.password);
+
+            if (match) {
+                res.send('Login successful!');
+            } else {
+                res.status(401).send('Invalid username or password');
+            }
+        } else {
+            res.status(401).send('Invalid username or password');
+        }
+    });
+});
+
+// Create user route
+app.post('/users', async (req, res) => {
+    const { username, password } = req.body;
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    db.query('INSERT INTO users (username, password) VALUES (?, ?)', [username, hashedPassword], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        res.status(201).send('User created successfully');
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
